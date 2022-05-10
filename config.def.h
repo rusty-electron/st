ead779c16bacc4657b32817a3a8aaadce2431e98
rusty-electron <pritom.gogoi101@gmail.com>
2022-05-10 13:20:26 +0530

    new changes

--- config.def.h
+++ config.def.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
-static char *font2[] = { "Symbola:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
+                         "Symbola:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
